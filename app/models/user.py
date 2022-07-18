862ebeb4aaae390c344de0248dfa99e729bb7201
Bojie Li <bojieli@gmail.com>
2022-07-18 19:54:27 +0800

    recompute number of comments on adding/deleting followers

--- app/models/user.py
+++ app/models/user.py
@@ -318,8 +318,8 @@ class User(db.Model, UserMixin):
         if followed in self.users_following:
             return False
         self.users_following.append(followed)
-        self.following_count += 1
-        followed.follower_count += 1
+        self.following_count = len(self.users_following)
+        followed.follower_count = len(followed.followers)
         db.session.commit()
         return True
 
@@ -327,8 +327,8 @@ class User(db.Model, UserMixin):
         if followed not in self.users_following:
             return False
         self.users_following.remove(followed)
-        self.following_count -= 1
-        followed.follower_count -= 1
+        self.following_count = len(self.users_following)
+        followed.follower_count = len(followed.followers)
         db.session.commit()
         return True
 
